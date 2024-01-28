fb938041088059a71d7579dfe28773ccf1673684
andre <andre@aSUS>
2024-01-28 03:11:57 +0300

    plus-minus

--- LargeNumbers.cpp
+++ LargeNumbers.cpp
@@ -113,37 +113,104 @@ std::string LargeNumbers::LargeNumber::toString() const {
 }
 
 LargeNumbers::LargeNumber LargeNumbers::LargeNumber::operator-() const {
-    LargeNumbers::LargeNumber res = LargeNumber();
+    LargeNumbers::LargeNumber res;
     res.sign = -sign;
     res.significand = significand;
     res.offset = offset;
     return res;
 }
 
-//LargeNumbers::LargeNumber LargeNumbers::LargeNumber::operator+(const LargeNumbers::LargeNumber &other) const {
-//    if (this->sign == -1) {
-//        return other - (-*this);
-//    }
-//    if (other.sign == -1) {
-//        return *this - (-other);
-//    }
-//
-//    size_t thisSize = this->significand.size();
-//    size_t otherSize = other.significand.size();
-//    size_t thisPrec = thisSize - this->offset;
-//    size_t otherPrec = otherSize - other.offset;
-//    size_t resSize = std::max(this->offset, other.offset) + std::max(thisPrec, otherPrec) + 1;
-//
-//    LargeNumbers::LargeNumber res;
-//    res.sign = 1;
-//    res.offset = std::max(this->offset, other.offset) + 1;
-//    res.significand = std::vector<int> (resSize, 0);
-//
-//
-//}
+LargeNumbers::LargeNumber LargeNumbers::LargeNumber::operator+(const LargeNumbers::LargeNumber &other) const {
+    if (sign != other.sign) {
+        return *this - (-other);
+    }
+
+    long long firstOffset = offset;
+    long long secondOffset = other.offset;
+    std::vector<int> first(significand);
+    std::vector<int> second(other.significand);
+    int maxOffset = std::max(offset, other.offset);
+
+    while (firstOffset < maxOffset) {
+        first.push_back(0);
+        firstOffset++;
+    }
+    while (secondOffset < maxOffset) {
+        second.push_back(0);
+        secondOffset++;
+    }
+    size_t maxSize = std::max(first.size(), second.size());
+    while (first.size() < maxSize) {
+        first.insert(first.begin(), 0);
+    }
+    while (second.size() < maxSize) {
+        second.insert(second.begin(), 0);
+    }
+
+    size_t resSize = maxSize + 1;
+    LargeNumbers::LargeNumber res;
+    res.sign = sign;
+    res.offset = maxOffset + 1;
+    res.significand = std::vector<int> (resSize, 0);
+
+    for (size_t i = 0; i < maxSize; i++) {
+        res.significand[i] += first[i] + second[i];
+        res.significand[i + 1] += res.significand[i] / 10;
+        res.significand[i] %= 10;
+    }
+    res.removeZeros();
+    return res;
+}
 
 LargeNumbers::LargeNumber LargeNumbers::LargeNumber::operator-(const LargeNumbers::LargeNumber &other) const {
-    return LargeNumbers::LargeNumber();
+    if (sign != other.sign) {
+        return *this + (-other);
+    }
+    if (sign == -1) {
+        return -other - (-*this);
+    }
+    if (other > *this) {
+        return -(other - *this);
+    }
+
+    long long firstOffset = offset;
+    long long secondOffset = other.offset;
+    std::vector<int> first(significand);
+    std::vector<int> second(other.significand);
+    int maxOffset = std::max(offset, other.offset);
+
+    while (firstOffset < maxOffset) {
+        first.push_back(0);
+        firstOffset++;
+    }
+    while (secondOffset < maxOffset) {
+        second.push_back(0);
+        secondOffset++;
+    }
+    size_t maxSize = std::max(first.size(), second.size());
+    while (first.size() < maxSize) {
+        first.insert(first.begin(), 0);
+    }
+    while (second.size() < maxSize) {
+        second.insert(second.begin(), 0);
+    }
+
+    size_t resSize = maxSize;
+    LargeNumbers::LargeNumber res;
+    res.sign = 1;
+    res.offset = maxOffset;
+    res.significand = std::vector<int> (resSize, 0);
+
+    for (size_t i = 0; i < maxSize; ++i) {
+        res.significand[i] = first[i] - second[i];
+        if (res.significand[i] < 0) {
+            first[i + 1]--;
+            res.significand[i] += 10;
+        }
+    }
+
+    res.removeZeros();
+    return res;
 }
 
 LargeNumbers::LargeNumber LargeNumbers::LargeNumber::operator*(const LargeNumbers::LargeNumber &other) const {
@@ -155,29 +222,29 @@ LargeNumbers::LargeNumber LargeNumbers::LargeNumber::operator/(const LargeNumber
 }
 
 bool LargeNumbers::LargeNumber::operator>(const LargeNumbers::LargeNumber &other) const {
-    if (this->sign != other.sign) {
-        return this->sign > other.sign;
+    if (sign != other.sign) {
+        return sign > other.sign;
     }
-    if (this->sign == -1) {
+    if (sign == -1) {
         return -*this < -other;
     }
 
-    if (this->significand.size() == 1 && this->significand[0] == 0 ||
+    if (significand.size() == 1 && significand[0] == 0 ||
         other.significand.size() == 1 && other.significand[0] == 0) {
-        return !(this->significand.size() == 1 && this->significand[0] == 0);
+        return !(significand.size() == 1 && significand[0] == 0);
     }
 
-    if (this->offset != other.offset) {
-        return this->offset > other.offset;
+    if (offset != other.offset) {
+        return offset > other.offset;
     }
 
     size_t i = 0;
-    size_t minSize = std::min(this->significand.size(), other.significand.size());
-    size_t thisSize = this->significand.size();
+    size_t minSize = std::min(significand.size(), other.significand.size());
+    size_t thisSize = significand.size();
     size_t otherSize = other.significand.size();
     for (; i < minSize; ++i) {
-        if (this->significand[thisSize - 1 - i] != other.significand[otherSize - 1 - i]) {
-            return this->significand[thisSize - 1 - i] > other.significand[otherSize - 1 - i];
+        if (significand[thisSize - 1 - i] != other.significand[otherSize - 1 - i]) {
+            return significand[thisSize - 1 - i] > other.significand[otherSize - 1 - i];
         }
     }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 using namespace LargeNumbers::literals;
 
 int main() {
-    LargeNumbers::LargeNumber a = "001300.011235000"_LN;
-    LargeNumbers::LargeNumber b = a;
-    std::cout << (-a > b) << std::endl;
+    LargeNumbers::LargeNumber a = "-01300.11523000"_LN;
+    LargeNumbers::LargeNumber b = "-0"_LN;
+    long double a1 = -1300.11523;
+    long double b1 = -0;
+    std::cout << a - b << std::endl;
+    std::cout << a1 - b1 << std::endl;
 }
