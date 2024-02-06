9faf490b013ecd4cd19c53db8cd6dd849e6faf65
andre <andre@aSUS>
2024-02-07 01:29:25 +0300

    Args to PI

--- computePi/compute.cpp
+++ computePi/compute.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 #include <ctime>
+#include <string>
 
 #include "LargeNumbers.h"
 
 using namespace LargeNumbers::literals;
 
 
-int main() {
-    LargeNumbers::setGlobalPrecision(1000);
+int main(int argc, char* argv[]) {
+    int precision = 100;
+    if (argc > 0) {
+        precision = std::stoi(std::string(argv[1]));
+    }
+    LargeNumbers::setGlobalPrecision(precision);
 
     std::clock_t start = std::clock();
     LargeNumbers::LargeNumber myPi = LargeNumbers::getPi();
@@ -16,4 +21,5 @@ int main() {
     std::cout << myPi << std::endl;
     std::cout << "Precision: " << LargeNumbers::getGlobalPrecision() << std::endl;
     std::cout << 1000.0 * (end - start) / CLOCKS_PER_SEC << " ms" << std::endl;
+    return 0;
 }
